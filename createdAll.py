2ceefbbfbfd0e153dfd932e5f7cbe76dc0ab0e60
Richard Izip <richardizip@gmail.com>
2016-10-22 13:02:08 +0200

    Aaa

--- createdAll.py
+++ createdAll.py
@@ -18,7 +18,7 @@ def day_times(hour):
     else:
         return 0
 
-with open('timeFiltered.csv', newline='') as csvfile:
+with open('dataCreated.csv', newline='') as csvfile:
     reader = csv.reader(csvfile, delimiter=',', quotechar='|')
     i = 0
     for row in reader:
@@ -27,11 +27,10 @@ with open('timeFiltered.csv', newline='') as csvfile:
                 data.append(row)
             i += 1
 
-# use astype to avoid errors with different types than floats
 data = np.array(data)
-y = data[:, len(data[0, :])-1]
-X = data[:, 0:len(data[0, :])-1]
-ones = np.ones((len(X), 1))
+y = data[:, len(data[0, :])-1] # last columns
+X = data[:, 1:len(data[0, :])-1] # do not use day feature
+ones = np.ones((len(X), 1)) # add column of ones
 X = np.concatenate([ones, X], axis=1)
 
 # day time dummies
@@ -40,10 +39,10 @@ hours = np.array(list(map(day_times, hours)))
 hours_d = np.array(pd.get_dummies(hours))
 
 # author dummies
-author_d = np.array(pd.get_dummies(X[:, 3]))
+author_d = np.array(pd.get_dummies(X[:, 2]))
 
 # section dummies
-section_d = np.array(pd.get_dummies(X[:, 4]))
+section_d = np.array(pd.get_dummies(X[:, 3]))
 
 X = np.concatenate((X[:, 0:2], hours_d, author_d, section_d), axis=1)
 X = X.astype(np.float)
@@ -66,28 +65,28 @@ error_r = np.sqrt(np.mean(res**2))
 print('Mean error', error_m)
 print('Root squared error', error_r)
 
-# Use for first hour clicks only
-'''
-plt.figure(1)
-plt.plot(X_test[:, 1], y_predicted, 'or', label='Predicted values')
-plt.plot(X_test[:, 1], y_test, 'ok', label='Test values')
-plt.legend(loc=2)
-plt.title('Linear regression (Created data)')
-plt.xlabel('Clicks 1st hour')
-plt.ylabel('Total clicks')
-plt.show()
-'''
+# some values are predicted weird negative results
+predicted = []
+for i in list(y_predicted):
+    if i > 0:
+        predicted.append(i)
+    else:
+        predicted.append(0)
+
+predicted = np.array(predicted)
+res = abs(y_test - predicted)
+error_m = np.mean(res)
+error_r = np.sqrt(np.mean(res**2))
+print('Mean error (remove negatives)', error_m)
+print('Root squared error (remove negatives)', error_r)
 
 # Use when article is created
 plt.figure(1)
-#plt.plot(y_test, X_test[:, 1], 'ok', label='Test values')
-plt.plot(y_predicted, 'og', label='Predicted values')
+plt.plot(y_test, 'ok', label='Test values')
+plt.plot(predicted, 'or', label='Predicted values')
 plt.legend(loc=1)
-#plt.axes([0, 2000, -20, 20000])
 plt.title('Linear regression (Created data)')
-plt.xlabel('Day time')
+plt.xlabel('Page view id')
 plt.ylabel('Total clicks')
 plt.show()
-
-print(y_predicted[0:200])
 
